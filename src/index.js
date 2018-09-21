56e852669572d0c010c7a577fcf9956eccb62ed3
Lesechka <littlecare@mail.ru>
2018-09-21 06:44:34 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
+function isOpened(element, bracketsConfig) {
+  return bracketsConfig.some(pair => pair[0] === element);
+}
+function isClosed(element, bracketsConfig) {
+  return bracketsConfig.some(pair => pair[1] === element);
+}
+function getOpposite(element, bracketsConfig) {
+  const first = bracketsConfig.find((config) => config[0] === element);
+  const second = bracketsConfig.find((config) => config[1] === element);
+  return (first && first[1]) || (second && second[0]);
+}
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+    const bracket = str[i];
+    const last = stack[stack.length-1];
+
+    if (!last) {
+      stack.push(bracket);
+      continue;
+    }
+    const isClosedBracket = isClosed(bracket, bracketsConfig);
+    const isOpenedBracket = isOpened(bracket, bracketsConfig);
+    const opposite = getOpposite(bracket, bracketsConfig);
+
+    if (opposite === bracket && last === bracket) {
+      stack.pop();
+      continue;
+    }
+    if (isOpenedBracket) {
+      stack.push(bracket);
+      continue;
+    }
+    if (isClosedBracket) {
+      if (getOpposite(last, bracketsConfig) !== bracket) {
+        return false;
+      } else {
+        stack.pop();
+        continue;
+      }
+  }
+}
+return stack.length === 0;
 }
